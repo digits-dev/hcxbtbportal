80e227b10df2eab34760b5507e8aa6e698e1bb8b
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-07-29 10:52:26 +0800

    update: add status model

---
+++ app/Models/Statuses.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Statuses extends Model
+{
+    use HasFactory;
+
+    public const FOR_VERIFICATION = 1;
+    public const CONFIRMED = 2;
+}
