a925edda50b41be32adf5f9ef82d811e739b075c
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-08-06 09:49:15 +0800

    update: mode of payments migration

---
+++ app/Models/ModeOfPayments.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModeOfPayments extends Model
+{
+    use HasFactory;
+
+    Public const CASH_ON_DELIVERY = 1;
+    Public const GRAB_PAY = 2;
+    Public const GCASH = 3;
+    Public const BANK_DEPOSIT = 4;
+    Public const LOYALTY_POINTS = 5;
+    Public const CREDIT_DEBIT_CARD = 6;
+    Public const NA = 7;
+    Public const OTHER = 8;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'color',
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $filterable = [
+        'name',
+        'color',
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+    ];
+
+       public function scopeSearchAndFilter($query, $request){
+
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $query->where(function ($query) use ($search) {
+                foreach ($this->filterable as $field) {
+                    if (in_array($field, ['created_at', 'updated_at'])) {
+                        $query->orWhereDate($field, $search);
+                    }
+                    else {
+                        $query->orWhere($field, 'LIKE', "%$search%");
+                    }
+                }
+            });
+        }
+
+        foreach ($this->filterable as $field) {
+
+            if ($request->filled($field)) {
+                   if ($request->filled($field)) {
+                    $data = $request->input($field);
+                    $operator = $data['operator'] ?? '=';
+                    $sorting = strtolower($data['sorting'] ?? '') === 'ascending' ? 'asc' : 'desc';
+                    $value = $data['value'] ?? null;
+
+                   switch ($operator) {
+                        case 'Empty (or Null)':
+                            $query->whereNull($field);
+                            break;
+    
+                        case 'NOT IN':
+                            $query->whereNotIn($field, is_array($value) ? $value : explode(',', $value));
+                            break;
+    
+                        case 'IN':
+                            $query->whereIn($field, is_array($value) ? $value : explode(',', $value));
+                            break;
+    
+                        case '!= (Not Equal to)':
+                            $query->where($field, '!=', $value);
+                            break;
+    
+                        case '= (Equal to)':
+                            $query->where($field, '=', $value);
+                            break;
+    
+                        case 'NOT LIKE':
+                            $query->where($field, 'NOT LIKE', "%$value%");
+                            break;
+    
+                        case 'LIKE':
+                        default:
+                            $query->where($field, 'LIKE', "%$value%");
+                            break;
+                    }
+
+                // Optional sorting logic
+                if (!empty($data['sorting'])) {
+                    $query->orderBy($field, $sorting);
+                }
+
+            }
+            }
+        }
+    
+        return $query;
+        
+    }
+
+    public function getCreatedBy() {
+        return $this->belongsTo(AdmUser::class, 'created_by', 'id');
+    }
+    
+    public function getUpdatedBy() {
+        return $this->belongsTo(AdmUser::class, 'updated_by', 'id');
+    }
+}
---
+++ resources/js/Pages/ModeOfPayments/ModeOfPayments.jsx
@@ -0,0 +1,14 @@
+import { Head, Link, router, usePage } from '@inertiajs/react';
+                    import React, { useState } from 'react';
+                    import ContentPanel from '../../Components/Table/ContentPanel';
+                    const ModeOfPayments = () => {
+                        return(
+                            <>
+                                <ContentPanel>
+                                    <div>This is ModeOfPayments module table area</div>
+                                </ContentPanel>
+                            </>
+                        );
+                    };
+
+                    export default ModeOfPayments;
